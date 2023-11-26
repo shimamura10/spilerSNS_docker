5381d7b8c629c8b93f2462fc6e8b4b3029c10c8d
shimamura10 <kshimamura1717@gmail.com>
2023-11-26 14:52:09 +0000

    add debugbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (app()->isProduction()) {
-            \URL::forceScheme("https");
+        if (app()->isProduction() || $this->app->environment() === "staging") {
+            \URL::forceScheme("https"); 
         }
     }
 }
